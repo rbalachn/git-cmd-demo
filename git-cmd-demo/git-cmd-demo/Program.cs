0fb8a3d46dbf73cc8ff07ae0134813fd5d933880
Balakrishnan R <balakrishnan@idiq.com>
2022-04-27 13:00:34 +0530

    from github 2

--- git-cmd-demo/git-cmd-demo/Program.cs
+++ git-cmd-demo/git-cmd-demo/Program.cs
@@ -9,6 +9,7 @@ namespace git_cmd_demo
             Console.WriteLine("Hello World!");
             Console.WriteLine("From github");
             Console.WriteLine("from local machine");
+             Console.WriteLine("From github 2");
         }
     }
 }
