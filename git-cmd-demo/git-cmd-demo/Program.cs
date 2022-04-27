27b87b0347cae6864221ddc8eb7378c3c75599a2
Balakrishnan R <balakrishnan@idiq.com>
2022-04-27 12:43:18 +0530

    release update 1

--- git-cmd-demo/git-cmd-demo/Program.cs
+++ git-cmd-demo/git-cmd-demo/Program.cs
@@ -7,7 +7,6 @@ namespace git_cmd_demo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("From github");
             Console.WriteLine("from local machine");
         }
     }
