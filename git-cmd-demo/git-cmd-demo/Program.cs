435f8976b4035b3816352f459f0daab7fc692175
Balakrishnan R <balakrishnan@idiq.com>
2022-04-27 12:53:41 +0530

    feature update 2 combined

--- git-cmd-demo/git-cmd-demo/Program.cs
+++ git-cmd-demo/git-cmd-demo/Program.cs
@@ -8,6 +8,11 @@ namespace git_cmd_demo
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("from local machine");
+            Console.WriteLine("from local machine 1");
+            Console.WriteLine("from local machine 2");
+            Console.WriteLine("from local machine 3");
+            Console.WriteLine("from local machine 4");
+            Console.WriteLine("from local machine 5");
         }
     }
 }
