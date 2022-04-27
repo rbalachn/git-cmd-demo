b0fd76095ee9bf106ea977ce66e2d1de3f66e3d8
Balakrishnan R <balakrishnan@idiq.com>
2022-04-27 10:57:51 +0530

    initialization

---
+++ git-cmd-demo/git-cmd-demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace git_cmd_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
